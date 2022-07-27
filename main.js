f5dc79dcf0248a40f28513eab96331f5be8eba43
HDuvier <hectord_castro@hotmail.com>
2022-07-27 17:05:03 -0500

    se hacen cambios en los menus mobile y desktop

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const menuMobile = document.querySelector('.mobile-menu');
+const shoppingCar = document.querySelector('.navbar-shopping-cart');
+const menuShoppingCar = document.querySelector('.product-detail')
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click",toggleMobileMenu)
+shoppingCar.addEventListener("click",toggleCarMenu);
 
 function toggleDesktopMenu() {
+  menuShoppingCar.classList.add('inactive');
   desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+  menuShoppingCar.classList.add('inactive');
+  menuMobile.classList.toggle('menu');
+}
+
+function toggleCarMenu(){
+  menuMobile.classList.add('inactive');
+  desktopMenu.classList.add('inactive');
+  menuShoppingCar.classList.toggle('inactive')
 }
 
